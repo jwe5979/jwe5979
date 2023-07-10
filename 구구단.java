23afc6af301b951082b9b7753f392ffcb146af73
jwe5979 <139097786+jwe5979@users.noreply.github.com>
2023-07-10 14:53:56 +0900

    Update 구구단.java

--- 구구단.java
+++ 구구단.java
@@ -1,16 +1,16 @@
 //전철우
 
-public class Main {
+public class Mini {
 
     public static void main(String[] args) {
         System.out.println("[구구단 출력]");
 
-                for (int i = 1; i <= 9; i++) {
-                    for (int j = 1; j <= 9; j++) {
-                        System.out.print(j + "*" + i + "=" + String.format("%3d", i * j));
-                        System.out.print("   ");
-                    }
-                    System.out.println();
-                }
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(String.format("%02d",j) + "*" + String.format("%02d",i) + "=" + String.format("%02d", i * j));
+                System.out.print("   ");
+            }
+            System.out.println();
         }
     }
+}
